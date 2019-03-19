9786af5de7648f238e48122f162e802a276cbcb9
Tolani Oyefule <Tolani.Oyefule@gmail.com>
2019-03-19 17:10:55 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBootPagePermissionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPagePermissionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPagePermissionsApplication.class, args);
+    }
+
+}
